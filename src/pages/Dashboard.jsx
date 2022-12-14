0a6399ed86718992e519e619ccb8a52c7b894807
jemish <jemishmalaviya35@gmail.com>
2022-12-14 10:08:06 +0530

    added new side menu

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 // import Sidebar from "../components/Sidebar";
-import SideMenu from "../components/Sidebar2";
-import { Row, Col, Container } from "react-bootstrap";
+// import SideMenu from "../components/Sidebar2";
+import MiniDrawer from "../components/Sidebar3";
+// import { Row, Col, Container } from "react-bootstrap";
 
 const Dashboard = () => {
   return (
     <>
-      <Container fluid>
+      {/* <Container fluid>
         <Row>
-          <Col className="ps-0" sm={12} md={3} lg={2}>
-            {/* <Sidebar /> */}
-            <SideMenu />
-          </Col>
+          <Col className="ps-0" sm={12} md={3} lg={2}></Col>
           <Col className="p-0" sm={12} md={9} lg={10}></Col>
         </Row>
-      </Container>
+      </Container> */}
+      <MiniDrawer />
     </>
   );
 };
